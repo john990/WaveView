23875bfa63d3c63c3da24571c7041c529d5c6a51
kai.wang <kai.wang.john@gmail.com>
2014-06-17 17:06:19 +0800

    update demo

--- demo/src/com/waveview/demo/MainActivity.java
+++ demo/src/com/waveview/demo/MainActivity.java
@@ -2,15 +2,40 @@ package com.waveview.demo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+
+import com.john.waveview.WaveView;
 
 /**
  * Created by kai.wang on 6/17/14.
  */
 public class MainActivity extends Activity {
 
+    private SeekBar seekBar;
+    private WaveView waveView;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        seekBar = (SeekBar) findViewById(R.id.seek_bar);
+        waveView = (WaveView) findViewById(R.id.wave_view);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                waveView.setProgress(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
